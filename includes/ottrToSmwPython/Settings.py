a1658b9c7b11d1110b4fe84096b9eafe1b4f9ba7
Oliver Tautz <oliver.tautz@uni-bielefeld.de>
2023-02-02 15:32:37 +0100

    remove namespaces from settings for vanilla image

--- includes/ottrToSmwPython/Settings.py
+++ includes/ottrToSmwPython/Settings.py
@@ -3,7 +3,7 @@ Default_Namespaces = ['Main','Template']
 
 # acceptable namespaces for ottr_templates
 # add yours to surpress warning
-ottr_template_namespaces = ('dpm','template','dpmtest')
+ottr_template_namespaces = ('template')
 
 # typehint for ottr types ... will be printed next to the type in forms
 form_typehint_mapping = {'xsd:date': "--date-format = (YYYY-MM-DD)"}
