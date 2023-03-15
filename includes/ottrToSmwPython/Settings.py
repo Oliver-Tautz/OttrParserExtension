3841cb230324607fb9a817f567ccb8e59ec9c005
Oliver Tautz <oliver.tautz@uni-bielefeld.de>
2023-03-15 18:59:23 +0100

    small settings.py doc change

--- includes/ottrToSmwPython/Settings.py
+++ includes/ottrToSmwPython/Settings.py
@@ -1,9 +1,10 @@
 
 #Acceptable namespaces for ottr_templates.
 #Add yours to call ottr templates from these namespaces.
-#The namespaces also need to be introduced in the LocalSettings.php in your wiki to work correctly!
-#Namespaces are case-sensitive!
 
+#The namespaces also need to be introduced in the LocalSettings.php in your wiki to work correctly.
+
+#Namespaces are case-sensitive!
 ottr_template_namespaces = ('Template','Dpm')
 
 # Dictionary of typehints. You can define hints for arbitrary types here. they will be shown to the user on form pages.
